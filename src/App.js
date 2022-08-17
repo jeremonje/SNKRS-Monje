42313548d81dffbd1906cdd443932f3e384cee97
jeremonje <jeremias17833@gmail.com>
2022-08-17 16:39:32 -0300

    modificamos app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Alerta from './components/Alerta/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Alerta></Alerta>
+      </header>
+    </div>
+  );
+}
+/*
+<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Learn React 1
+        </a>*/
 
 export default App;
---
+++ src/components/Alerta/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css';
+
+function Alerta(){
+    return(
+        
+        <header>
+            <h1>yourStyle</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">What's New</a></li>
+                <li><a href="#">Newsletter</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Alerta;
